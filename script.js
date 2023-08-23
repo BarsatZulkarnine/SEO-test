19abc3e803d5edb8e3204914720e259860bc6ca6
Barsatzulkarnine <mohammedbarsatzulkarnine@gmail.com>
2023-08-23 18:36:39 +1000

    somechagne

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
+const symptomToDiagnosis = {
+    "fever": "Common cold",
+    "cough": "Bronchitis",
+    "diarrhea": "Gastroenteritis",
+    "vomiting": "Food poisoning",
+    // Add more symptoms and diagnoses here
+};
+
 function searchResources() {
-    const searchInput = document.getElementById("searchInput").value;
+    const searchInput = document.getElementById("searchInput").value.toLowerCase();
     const resultsContainer = document.getElementById("resultsContainer");
 
-    // Simulating an API call with mock data
-    const mockApiResponse = [
-        "Resource 1",
-        "Resource 2",
-        "Resource 3"
-    ];
-
-    // Clear previous results
     resultsContainer.innerHTML = "";
 
-    // Display results
-    mockApiResponse.forEach(resource => {
+    if (symptomToDiagnosis[searchInput]) {
         const resultItem = document.createElement("div");
         resultItem.className = "result-item";
-        resultItem.innerText = resource;
+        resultItem.innerText = `Probable diagnosis for "${searchInput}": ${symptomToDiagnosis[searchInput]}`;
         resultsContainer.appendChild(resultItem);
-    });
+    } else {
+        const resultItem = document.createElement("div");
+        resultItem.className = "result-item";
+        resultItem.innerText = "No probable diagnosis found for the entered symptom.";
+        resultsContainer.appendChild(resultItem);
+    }
 }
